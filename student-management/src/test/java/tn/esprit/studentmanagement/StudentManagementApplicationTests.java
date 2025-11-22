ed20d1551de59b9c8bacf23ce11cc7e6f53c76fc
AMENIFAKHRI <FAKHRI.AMENI@esprit.tn>
2025-11-22 14:46:02 +0100

    FIX FINAL : @ActiveProfiles('test') + application-test.properties

--- student-management/src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ student-management/src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,12 +2,13 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")   // CETTE LIGNE EST LA CLÉ DE TOUT
 class StudentManagementApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
